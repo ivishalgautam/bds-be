2adc85fe8719d5102db5aee3872b35bb5b4608f0
Vishal Gautam <ivishalgautamm@gmail.com>
2023-12-30 14:03:40 +0530

    added video compressor

--- app/config/s3Service.js
+++ app/config/s3Service.js
@@ -4,6 +4,7 @@ import {
   PutObjectCommand,
   S3Client,
 } from "@aws-sdk/client-s3";
+import Ffmpeg from "fluent-ffmpeg";
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
@@ -19,11 +20,25 @@ const uploadToS3 = async (file, folder) => {
     .replaceAll("'", "_")
     .replaceAll("/", "_")}`;
 
+  const compressedFilePath = `temp/${uuidv4()}-${file.filename}`;
+  // Perform video compression using ffmpeg
+  await new Promise((resolve, reject) => {
+    Ffmpeg(file.file)
+      .videoCodec("libx264")
+      .audioCodec("aac")
+      .outputOptions("-crf 28")
+      .on("end", resolve)
+      .on("error", reject)
+      .save(compressedFilePath);
+  });
+
+  const compressedFileBuffer = await fs.promises.readFile(compressedFilePath);
+
   const command = new PutObjectCommand({
     Bucket: process.env.AWS_BUCKET_NAME,
     Key: key,
     ContentType: file.mimetype,
-    Body: await file.toBuffer(),
+    Body: compressedFileBuffer,
   });
 
   try {
